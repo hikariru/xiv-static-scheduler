540b989ef6669014e1f84c833e0b767f19d4be83
Uchiru <7587023+hikariru@users.noreply.github.com>
2022-07-30 11:24:19 +0900

    migration

---
+++ src/app/migration/1659147740361-migration.ts
@@ -0,0 +1,18 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class migration1659147740361 implements MigrationInterface {
+  name = 'migration1659147740361'
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "player"
+      ADD "ulid" CHARACTER VARYING(255) NOT NULL`);
+    await queryRunner.query(`ALTER TABLE "player"
+      ADD CONSTRAINT "UQ_7a80666a73fd2bbac3016735c67" UNIQUE ("ulid")`);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "player" DROP CONSTRAINT "UQ_7a80666a73fd2bbac3016735c67"`);
+    await queryRunner.query(`ALTER TABLE "player" DROP COLUMN "ulid"`);
+  }
+
+}
